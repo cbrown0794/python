e4a815ba2b9a995ca51f017af98495a8ec0cb8b2
Connor Brown <cbrown0794@floridapoly.edu>
2026-01-16 19:21:15 +0000

    1-16-2026

---
+++ 1-16-26/print_test.py
@@ -0,0 +1,5 @@
+my_name = "Connor Brown"
+print(my_name)
+print("Use 4 spaces per indentation level")
+print("Python code in one module gains access to the code in another module by the process of importing it")
+print("I want to get better at Python, code a project, and be able to expand my knowledge of programming through this course")
